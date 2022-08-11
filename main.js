c7a65ef2359cc9e4974cb62279eb4f41c207652e
lusi pri hartini <lusipr142@gmail.com>
2022-08-11 21:20:37 +0700

    add testimonial

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+{
+    "dependencies": {
+      "parcel-bundler": "^1.6.1",
+      "swiper": "latest"
+    },
+    "name": "Swiper - Pagination progress",
+    "tags": [
+      "swiper"
+    ]
+  }
