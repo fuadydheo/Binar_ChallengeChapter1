eca3df10e6f156a2413ab52ce7c75106e9db1b46
unknown <irvanrizky58@gmail.com>
2022-08-11 17:34:22 +0700

    media query tablet

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// change navbar styles on scroll
-
-window.addEventListener('scroll',() => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY>0)
-})
-
 //show/hide faq answer
 
 const faqs = document.querySelectorAll('.faq');
@@ -22,3 +15,23 @@ faqs.forEach(faq => {
         }
     })
 })
+
+//show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display= "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close navmenu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block"
+}
+
+closeBtn.addEventListener('click', closeNav)
