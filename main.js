982bd2b8b3c846270e98b251704c6882d0a57de5
Andarios910 <andarioskarokaro92@gmail.com>
2022-08-11 15:44:33 +0700

    repair FAQ and add main.js, add swiper testimonials

---
+++ main.js
@@ -0,0 +1,14 @@
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq__icon img');
+        if (icon.style.transform === '') {
+            icon.style.transform = 'rotate(180deg)';
+        } else {
+            icon.style.transform = '';
+        }
+    })
+})
