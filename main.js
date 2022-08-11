8d267299b5f284f555e54d3a6f92c64a6a9d7dbf
kautsar07 <kautsar07777@gmail.com>
2022-08-11 16:45:12 +0700

    add faq and testimonial

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ window.addEventListener('scroll', ()=> {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 100)
 })
+
+// show /hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        
+        //change icon
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'uil uil-angle-down') {
+            icon.className = "uil uil-angle-up"
+        } else{
+            icon.className = "uil uil-angle-down";
+        }
+        
+    })
+})
