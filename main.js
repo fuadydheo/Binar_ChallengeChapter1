bc6b490de99890088adf9f885d569a6cbe2d7970
azkinf16 <azkifajri16@gmail.com>
2022-08-12 09:00:57 +0700

    add side navbar on media phone

---
+++ main.js
@@ -0,0 +1,9 @@
+// show open and close nav
+
+function openNav() {
+    document.getElementById("side_nav").style.width = "50%";
+}
+
+function closeNav() {
+    document.getElementById("side_nav").style.width = "0%";
+}
