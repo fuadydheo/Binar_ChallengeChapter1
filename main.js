57ccc54669097cd74babb8ddaf51be1da335aa7d
Kris Adiwinata <krisadiwinata12@gmail.com>
2022-08-12 18:31:25 +0700

    Add responsive Media

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ window.addEventListener('scroll',()=> {
     ('window-scroll', window.scrollY > 0);
 })
 
+//show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close
+const closeNav = () =>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
 
 //Show/Hide FAQ
 const faqs = document.querySelectorAll('.faq');
@@ -21,3 +40,4 @@ faqs.forEach(faq =>{
         }
     })
 })
+
