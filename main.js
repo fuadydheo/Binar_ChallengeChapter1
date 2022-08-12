f07299be750d6a88af272f68c0bbb2e970c19586
Kris Adiwinata <krisadiwinata12@gmail.com>
2022-08-12 17:41:05 +0700

    added navScroll & faqScroll JS

---
+++ main.js
@@ -0,0 +1,23 @@
+// Nav Scroll
+window.addEventListener('scroll',()=> {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0);
+})
+
+
+//Show/Hide FAQ
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-angle-down'){
+            icon.className = "uil uil-minus"
+        }else{
+            icon.className = "uil uil-angle-down"
+        }
+    })
+})
