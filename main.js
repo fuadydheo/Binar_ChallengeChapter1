fe3c7fb70c46d53850adbc8bd65ad802a0bcc40d
Andarios910 <andarioskarokaro92@gmail.com>
2022-08-12 14:41:40 +0700

    add navbar dropdown menu

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
-
-
-
+//FAQ
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
@@ -19,3 +17,22 @@ faqs.forEach(faq => {
         }
     })
 })
+
+//show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
