38f39dc325b98ab38d3e5211fb93962721ffda07
gihonhon <agunggihon@gmail.com>
2022-08-18 22:13:58 +0800

    fix Faqs

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+//FAQ SEXTION
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-angle-down') {
+      icon.className = 'uil uil-minus' 
+    } else {
+      icon.className = 'uil uil-angle-down'
+    }
+  })
+})
